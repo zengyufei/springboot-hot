84d7a8b12ab72d179cc4852b773678e01a40413a
zengyufei <31636208@qq.com>
2024-06-06 08:51:49 +0800

    增加测试

--- src/main/java/com/example/hot/entity/User.java
+++ src/main/java/com/example/hot/entity/User.java
@@ -1,8 +1,12 @@
 package com.example.hot.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private Long id;
     private String name;
--- src/test/java/com/example/hot/SpringHotApplicationTests.java
+++ src/test/java/com/example/hot/SpringHotApplicationTests.java
@@ -1,13 +1,57 @@
 package com.example.hot;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.example.hot.api.DemoController;
+import com.example.hot.entity.User;
+import com.example.hot.mapper.UserMapper;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
-class SpringHotApplicationTests {
+import java.util.Collections;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/**
+ * user controller test - use mockito.
+ */
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(value = DemoController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserMapper userMapper;
+
+    /**
+     * test find all user.
+     *
+     * @throws Exception exception
+     */
     @Test
-    void contextLoads() {
-    }
+    @DisplayName("Test selectList()")
+    public void list() throws Exception {
+        Mockito.when(userMapper.selectList(null)).thenReturn(
+                Collections.singletonList(new User(1L, "pdai.tech", 14, "1221111")));
 
+        mockMvc.perform(MockMvcRequestBuilders.get("/mp")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(userMapper, times(1)).selectList(null);
+    }
 }
